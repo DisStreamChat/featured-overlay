20436d24e6b1ae9905fc31d6507f9f67f77b6046
David Snyder <davidgraygs4@gmail.com>
2020-10-20 21:44:32 -0400

    added css and settings

--- src/App.js
+++ src/App.js
@@ -1,43 +1,48 @@
 import React, { useEffect, useState } from "react";
 import { Message } from "chatbits";
 import { TransitionGroup } from "react-transition-group";
-import "./App.css";
-import { useCollection } from "react-firebase-hooks/firestore";
+import "./App.scss";
+import { useCollection, useDocument } from "react-firebase-hooks/firestore";
 import firebase from "./firebase";
-import "chatbits/dist/index.css"
+import "chatbits/dist/index.css";
 
 function App() {
-    const [snapshot, loading, error] = useCollection(
-        firebase.db
-            .collection("messages")
-            .doc("dav1dsnyder404")
-            .collection("messages")
-            .orderBy("sentAt", "desc")
-    );
+	const [snapshot, loading, error] = useCollection(
+		firebase.db.collection("messages").doc("dav1dsnyder404").collection("messages").orderBy("sentAt", "desc")
+	);
 
-    const [messages, setMessages] = useState([]);
+	const [userSnapshot, userLoading, userError] = useDocument(firebase.db.collection("Streamers").doc("da8c156ed7b5ce71650ffaf9beb68d5edf7e21ab"));
+	const [messages, setMessages] = useState([]);
+	const [settings, setSettings] = useState({});
 
-    useEffect(() => {
-        if (snapshot) {
-            const docs = snapshot.docs.map((doc) => doc.data());
-            setMessages(docs);
-        }
-    }, [snapshot]);
+	useEffect(() => {
+		if (snapshot) {
+			const docs = snapshot.docs.map(doc => doc.data());
+			setMessages(docs);
+		}
+	}, [snapshot]);
 
-    console.log(snapshot, loading, error)
-    return (
-        <TransitionGroup>
-            {messages.map((message) => (
-                <Message
-                    index={message.id}
-                    streamerInfo={{}}
-                    msg={message}
-                    // isOverlay
-                    key={message.id}
-                />
-            ))}
-        </TransitionGroup>
-    );
+	useEffect(() => {
+		if (userSnapshot) {
+			const data = userSnapshot.data();
+			if (data) {
+				setSettings(data.appSettings);
+				// setMessages(docs);
+			}
+		}
+	}, [userSnapshot]);
+
+	console.log(settings);
+
+	return (
+		<div style={{ fontFamily: settings.Font, fontSize: `${settings.FontScaling || 1}rem` }}>
+			<TransitionGroup>
+				{messages.map(message => (
+					<Message index={message.id} streamerInfo={settings} msg={{ ...message, platform: "discord" }} isOverlay key={message.id} />
+				))}
+			</TransitionGroup>
+		</div>
+	);
 }
 
 export default App;
