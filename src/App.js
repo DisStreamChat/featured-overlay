0e507d2ffe9eb1c3204d9a5266dda22a8e6ac59f
David Snyder <davidgraygs4@gmail.com>
2020-11-14 13:33:54 -0500

    remove comment

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import firebase from "./firebase";
 import "chatbits/dist/index.css";
 
 function App() {
-	// 
 	const [snapshot, loading, error] = useCollection(
 		firebase.db.collection("featured-messages").doc(process.env.REACT_APP_ID).collection("messages").orderBy("sentAt", "desc")
 	);
